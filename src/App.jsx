a8d84076edeb9c49a50237e35aa1d31961e66abd
Houssem <souarih3@gmail.com>
2024-02-12 15:37:18 +0100

    W3 List in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
 import * as React from "react";
-function getTitle(title) {
-  return title 
-}
-export default function App(){
-  const title = "My app Title";
-  const myObject = {
-    name: "John Doe",
-    age: 30,
-    city: "New York",
-  };
+import './App.css'
+const list = [
+  {
+    title : "React", 
+    url : "http://reactjs.org/",
+    author : "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "http://reduxjs.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+function App(){
   return (
     <div>
-      <h1>{getTitle("Hello React")}</h1>
-      <label htmlFor="inputField">Input Field:</label>
-      <input type="text" id="inputField" name="inputField" />
-      <h2>My Object:</h2>
-      <pre>{JSON.stringify(myObject, null, 2)}</pre>
+      <h1>My hacker stories</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+      <hr />
+      <ul>
+        {list.map(function(item){
+          return <li key={item.objectID}>{item.title}</li>
+        })}
+      </ul>
+      <ul>
+        {list.map(function(item){
+          return(
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })};
+      </ul>
     </div>
-
-  )
+  );
 }
+
+export default App;
